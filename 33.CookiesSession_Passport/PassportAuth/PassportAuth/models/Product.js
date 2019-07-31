5b92722e27008a6c64be2d397bd86bca9986d5a3
UkrAndy <brila_andrij@ukr.net>
2019-07-31 07:45:23 +0300

    SimpleEx

--- 33.CookiesSession_Passport/PassportAuth/PassportAuth/models/Product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const userScheme = new Schema({
-    title: String,
-    price: Number,
-    count:Number},
-    {versionKey: false});
-const Product = mongoose.model("Product", userScheme);
-module.exports=Product;
